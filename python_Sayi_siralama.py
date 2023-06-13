ff1ea9e6ce5d7809e42ccaa8bf40225e4ccccdbc
AZAT DİCLE <75863129+azatdicle@users.noreply.github.com>
2023-06-13 20:43:42 +0300

    python proje dosyası eklendi
    python proje dosyası eklendi

---
+++ python_Sayi_siralama.py
@@ -0,0 +1,29 @@
+print("Kullanici adi:azat Sifre:dicle")
+kdGirdi=input("Kullanici adini Giriniz:")
+sfGirdi=input("Sifre Giriniz:")
+print()
+dtuser="azat"
+dtsifre="dicle"
+key=1
+liste=[]
+adet=int(input("Kac Tane Sayi Girmek isiyorsun:"))
+while key<=adet:
+    if dtuser==kdGirdi and sfGirdi==dtsifre:
+        sayi=0
+        sayi=int(input("{} Sayiyi Giriniz: ".format(key)))
+        liste.append(sayi)
+        key+=1
+    elif sfGirdi!=dtsifre and dtuser!=kdGirdi:
+        print("Girilen bilgiler tamami ile yanlis :(")
+        break
+    elif dtuser!=kdGirdi:
+        print("Kullanici adi yanlis :(")
+        break
+    elif sfGirdi!=dtsifre:
+        print("Sifre yanlis :(")
+        break
+    
+liste.sort()
+print("Girdiginiz sayilarin kucukten buyuge siralanmasi {}".format(liste))
+liste.reverse()
+print("Girdiginiz sayilarin buyukten kucugre siralanmasi {}".format(liste))
